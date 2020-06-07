f163bd393831f70cb8019c1257a892fe82968558
TheLegendHimself <58825123+TheLegendHimself@users.noreply.github.com>
2020-06-07 17:31:24 +0200

    Update createAccount.js

--- boi_js/createAccount.js
+++ boi_js/createAccount.js
@@ -24,14 +24,14 @@ const handleFormSubmit = event => {
 };
 const handleAnSubmit = event => {
 	event.preventDefault();
-	const data = formToJSON(form.elements);
-	var myJSON = JSON.stringify(data);
-	console.log(myJSON);
-	var xhr = new XMLHttpRequest();
-	var url = "http://localhost:8000/api/login";
-	xhr.open("GET", url, true);
-	xhr.setRequestHeader("Content-Type", "application/json");
-	xhr.send(myJSON)
+	const data2 = formToJSON(form1.elements);
+	var myJSON2 = JSON.stringify(data2);
+	console.log(myJSON2);
+	var xhr2 = new XMLHttpRequest();
+	var url2 = "http://localhost:8000/api/login";
+	xhr2.open("GET", url2, true);
+	xhr2.setRequestHeader("Content-Type", "application/json");
+	xhr2.send(myJSON2)
 };
 const form = document.getElementsByClassName('regform')[0];
 form.addEventListener('submit', handleFormSubmit);
