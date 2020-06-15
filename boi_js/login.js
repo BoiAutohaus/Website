a111004905665dd6d616ed9a3992af7986fccbf6
schrotle <65763511+schrotle@users.noreply.github.com>
2020-06-15 08:11:58 +0200

    gj

---
+++ boi_js/login.js
@@ -0,0 +1,29 @@
+const isValidElement = element => {
+    return element.name && element.value;
+  };
+  
+  const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+    // Make sure the element has the required properties.
+      if (isValidElement(element) ) {
+      data[element.name] = element.value;
+    }
+    return data;
+  }, {});
+  
+  
+  const handleFormSubmit = event => {
+      event.preventDefault();
+      const data = formToJSON(form.elements);
+      var myJSON = JSON.stringify(data);
+      console.log(myJSON);
+      var xhr = new XMLHttpRequest();
+      var url = "http://localhost:8000/api/register";
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.send(myJSON)
+  };
+  
+  const form = document.getElementsByClassName('regform')[0];
+  form.addEventListener('submit', handleFormSubmit);
+  
+  
