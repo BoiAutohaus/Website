5dd45fa33048d2e4fa05f33411482adb7e2d3d90
schrotle <65763511+schrotle@users.noreply.github.com>
2020-07-09 16:12:46 +0200

    added searchbar

--- boi_js/suche.js
+++ boi_js/suche.js
@@ -1,4 +1,4 @@
-const isValidElement = element => {
+const isValidElement = element => {  
     return element.name && element.value;
   };
   
@@ -34,11 +34,40 @@ const isValidElement = element => {
       
 			console.log("Bad request");
 			window.alert("Error: "  +  json2.nachricht);
-		}
-		
+    }}
+  }
+  
+
+  const handleFormSubmitIII = event => {    
+    event.preventDefault();
+    const data = formToJSON(form_searchbar.elements);
+    console.log(data);
+    var myJSON = JSON.stringify(data);
+    console.log(myJSON);
+    var xhr2 = new XMLHttpRequest();
+      
+    var url = "http://localhost:8000/api/auto/suche";
+    xhr2.open("POST", url, true);
+    xhr2.setRequestHeader("Content-Type", "application/json");
+    xhr2.send(myJSON)
+    xhr2.onreadystatechange = function () {
+    if (xhr2.readyState === 4 && xhr2.status === 200) {
+            var json1 = JSON.parse(xhr.responseText);
+            console.log("Success request");
+      //window.alert(data2.mail + ", Sie haben sich efolgreich angemeldet "+ json1.mail);
+    }
+    else if(xhr2.readyState === 4 && xhr2.status === 401) {
+      var json2 = JSON.parse(xhr2.responseText);
+        
+      console.log("Bad request");
+      window.alert("Error: "  +  json2.nachricht);
+    }    		
 	}
 };
 
+const form = document.getElementsByClassName('suchform')[0];
+form.addEventListener('submit', handleFormSubmitII);
+const form_searchbar = document.getElementsByClassName('form-inline')[0];
+console.log(form_searchbar);
+form_searchbar.addEventListener('submit', handleFormSubmitIII);
 
-  const form = document.getElementsByClassName('suchform')[0];
-  form.addEventListener('submit', handleFormSubmitII);
