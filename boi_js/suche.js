427d3a6f3e87b826ac9f24da42c82c6544c25058
schrotle <65763511+schrotle@users.noreply.github.com>
2020-07-10 15:45:34 +0200

    fix

--- boi_js/suche.js
+++ boi_js/suche.js
@@ -163,7 +163,7 @@ function renderCars(cars) {
 		
 		// Create Link to Buypage
 		const cardA = document.createElement("a");
-		cardA.href = "ZumDeal_BMWM340i.html";
+		cardA.href = "ZumDeal_"+cars[i].Marke + cars[i].Modell + ".html";
 		
 		// Append Button To Link
 		cardA.appendChild(cardButton);
