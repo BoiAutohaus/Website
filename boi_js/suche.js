03d4623d1c0930d872d78b3a3f3199ef3b1d735a
TheLegendHimself <58825123+TheLegendHimself@users.noreply.github.com>
2020-07-10 10:12:44 +0200

    Cleanup

--- boi_js/suche.js
+++ boi_js/suche.js
@@ -1,78 +1,3 @@
-const isValidElement = element => {  
-    return element.name && element.value;
-  };
-  
-  const formToJSON = elements => [].reduce.call(elements, (data, element) => {
-    // Make sure the element has the required properties.
-      if (isValidElement(element) ) {
-			
-		data[element.name] = element.value;
-
-    }
-    return data;
-  }, {});
-
-  const handleFormSubmitII = event => {
-    event.preventDefault();
-    const data = formToJSON(form.elements);
-    var myJSON = JSON.stringify(data);
-    console.log(myJSON);
-    var xhr = new XMLHttpRequest();
-    
-    var url = "http://localhost:8000/api/auto";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(myJSON)
-    xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-            var json1 = JSON.parse(xhr.responseText);
-            console.log("Success request");
-            renderCars(json1);
-			//window.alert(data2.mail + ", Sie haben sich efolgreich angemeldet "+ json1.mail);
-		}
-		else if(xhr.readyState === 4 && xhr.status === 401) {
-      var json2 = JSON.parse(xhr.responseText);
-      
-			console.log("Bad request");
-			window.alert("Error: "  +  json2.nachricht);
-    }}
-  }
-  
-
-  const handleFormSubmitIII = event => {    
-    event.preventDefault();
-    const data = formToJSON(form_searchbar.elements);
-    console.log(data);
-    var myJSON = JSON.stringify(data);
-    console.log(myJSON);
-    var xhr2 = new XMLHttpRequest();
-      
-    var url = "http://localhost:8000/api/auto/suche";
-    xhr2.open("POST", url, true);
-    xhr2.setRequestHeader("Content-Type", "application/json");
-    xhr2.send(myJSON)
-    xhr2.onreadystatechange = function () {
-    if (xhr2.readyState === 4 && xhr2.status === 200) {
-            var json1 = JSON.parse(xhr.responseText);
-            console.log("Success request");
-			console.log(json1);
-			renderCars(json1);
-    }
-    else if(xhr2.readyState === 4 && xhr2.status === 401) {
-      var json2 = JSON.parse(xhr2.responseText);
-        
-      console.log("Bad request");
-      window.alert("Error: "  +  json2.nachricht);
-    }    		
-	}
-};
-
-const form = document.getElementsByClassName('suchform')[0];
-form.addEventListener('submit', handleFormSubmitII);
-const form_searchbar = document.getElementsByClassName('form-inline')[0];
-console.log(form_searchbar);
-form_searchbar.addEventListener('submit', handleFormSubmitIII);
-
 
 function renderCars(cars) {
   //cars = JSON.stringify(cars);
@@ -120,7 +45,87 @@ function renderCars(cars) {
   //cardBody.html(cars.text);
   card.append(cardBody);
 
- 
+};
+
+
+
+
+const isValidElement = element => {  
+    return element.name && element.value;
+  };
+  
+  const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+    // Make sure the element has the required properties.
+      if (isValidElement(element) ) {
+			
+		data[element.name] = element.value;
+
+    }
+    return data;
+  }, {});
+
+const handleFormSubmitII = event => {
+	event.preventDefault();
+    const data = formToJSON(form.elements);
+    var myJSON = JSON.stringify(data);
+    console.log(myJSON);
+    var xhr = new XMLHttpRequest();
+    
+    var url = "http://localhost:8000/api/auto";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(myJSON)
+    xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+            var json1 = JSON.parse(xhr.responseText);
+            console.log("Success request");
+			console.log(json1);
+            renderCars(json1);
+		}
+		else if(xhr.readyState === 4 && xhr.status === 401) {
+			var json2 = JSON.parse(xhr.responseText);
+			console.log("Bad request");
+			window.alert("Error: "  +  json1.nachricht);
+		}
+	}
+};
+  
+
+const handleFormSubmitIII = event => {    
+	event.preventDefault();
+    const data = formToJSON(form_searchbar.elements);
+    console.log(data);
+    var myJSON = JSON.stringify(data);
+    console.log(myJSON);
+    var xhr2 = new XMLHttpRequest();
+      
+    var url = "http://localhost:8000/api/auto/suche";
+    xhr2.open("POST", url, true);
+    xhr2.setRequestHeader("Content-Type", "application/json");
+    xhr2.send(myJSON)
+    xhr2.onreadystatechange = function () {
+		if (xhr2.readyState === 4 && xhr2.status === 200) {
+			var json1 = JSON.parse(xhr.responseText);
+			console.log("Success request");
+			console.log(json1);
+			renderCars(json1);
+			}
+		else if(xhr2.readyState === 4 && xhr2.status === 401) {
+		  var json2 = JSON.parse(xhr2.responseText);  
+		  console.log("Bad request");
+		  window.alert("Error: "  +  json2.nachricht);
+		}    		
+	}
+};
+
+const form = document.getElementsByClassName('suchform')[0];
+form.addEventListener('submit', handleFormSubmitII);
+const form_searchbar = document.getElementsByClassName('form-inline')[0];
+//console.log(form_searchbar);
+form_searchbar.addEventListener('submit', handleFormSubmitIII);
+
+
+
   
   
   
