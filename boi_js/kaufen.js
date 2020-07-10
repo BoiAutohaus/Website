356837a942b9ffb338805946767c075f8cbb2215
schrotle <65763511+schrotle@users.noreply.github.com>
2020-07-10 16:45:47 +0200

    fix

--- boi_js/kaufen.js
+++ boi_js/kaufen.js
@@ -12,9 +12,10 @@ const isValidElement = element => {
   
   
   const handleFormSubmit = event => {
+      
       event.preventDefault();
       const data = formToJSON(form.elements);
-      var myJSON = JSON.stringify(data);
+      var myJSON = JSON.stringify(data);      
       console.log(myJSON);
       var xhr = new XMLHttpRequest();
       var url = "http://localhost:8000/api/buy";
